4dbda3c73dc006b3e7654b21e5c5c2e33373ec3a
mtsnobrega <msnobrega@aluno.riobrancofac.edu.br>
2023-08-24 22:38:48 -0300

    aula01

---
+++ aula01/main.js
@@ -0,0 +1,22 @@
+// console.log eu uso para mostrar uma mensagem ao usuario
+// são usados para relaizar comentarios de ajuda para progrmador que vai ler o codigo
+console.log("hello world");
+
+//alert exibe uma mensagem de alerta ao usuario 
+alert("Olá mundo");
+
+//para delcarar variaveis no JS podemos "var", "let" ou "const
+//sempre que usar const para declarar uma variavel o valor dela não pode ser alterado 
+const pi = 3.14;
+//pi = 3.5;
+
+var nome = "matheus";
+nome = "maria";
+alert(nome);
+
+//"VAr" é uma variavel global para ser usado no codigo inteiro 
+//"Let" é uma variavel local para ser usado em um pedaço ou um bloco do codigo 
+
+
+
+
